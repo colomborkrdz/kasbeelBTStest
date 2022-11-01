231e441f0b7f899e16c491c0ea896191278c2132
0xTranqui <thinkandchill11@gmail.com>
2022-11-01 07:03:41 -0700

    install arial narrow locally

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,12 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-  ]
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        arialNarrow: ['arialNarrow']
+      }
+    }
+  }
 }
